65aede663dd0cfc796511ccab8546ae4b7359355
gabrielleviary <wei.he@sjsu.edu>
2020-11-11 11:06:18 -0800

    Read file and print statistics

--- Code/wine_clustering.py
+++ Code/wine_clustering.py
@@ -1,1 +1,16 @@
 import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+import pandas as pd
+
+pd.set_option('display.max_columns', None)
+pd.options.display.float_format = "{:.2f}".format
+
+data = pd.read_csv("Data/wine.csv")
+# print(data.head())
+
+print(data.info())
+print ("No missing data in this data set.")
+statistics = data.describe()
+print(statistics)
